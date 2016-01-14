600f6703c0305c9ae7b651dfb0a248e4c73b507d
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-01-14 14:43:48 -0600

    refactor init.js to app.js

--- app/services/drawer.service.js
+++ app/services/drawer.service.js
@@ -5,6 +5,7 @@
 
 	var service = {
 		toggle: function() {
+			// close the navigation drawer when a lnk is clicked.
 			var drawer = documentService.querySelectorAll('.mdl-layout__drawer, .mdl-layout__obfuscator');
 			Array.prototype.forEach.call(drawer, function(child) {
 				child.classList.remove('is-visible');
