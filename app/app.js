94f4a88df82a94ca0634c087c4ee9dee6a99e1e9
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-01-15 07:40:33 -0600

    add DOMContentLoaded, nodeListService.

--- app/app.js
+++ app/app.js
@@ -5,31 +5,37 @@
 		var drawerService = require('services/drawer.service');
 		var eventService = require('services/event.service');
 		var incrementFactory = require('services/increment.factory');
+		var nodeListService = require('services/node.list.service');
 
 		var app = {
 			init: function() {
-				var increment = incrementFactory.create();
-				var count = documentService.getElementById('count');
 
-				eventService.addEventListener(documentService.getElementById('increment'), 'click', function() {
-					count.textContent = increment.increment();
-				});
+				eventService.addEventListener(document, 'DOMContentLoaded', addEvents);
 
-				eventService.addEventListener(documentService.getElementById('decrement'), 'click', function() {
-					count.textContent = increment.decrement();
-				});
+				function addEvents() {
+					var increment = incrementFactory.create();
+					var count = documentService.getElementById('count');
 
-				eventService.addEventListener(documentService.getElementById('reset'), 'click', function() {
-					count.textContent = increment.reset();
-				});
-
-				var drawer = documentService.getElementById('drawerNav');
-				Array.prototype.forEach.call(drawer.childNodes, function(child) {
-					eventService.addEventListener(child, 'click', function() {
-						drawerService.toggle();
+					eventService.addEventListener(documentService.getElementById('increment'), 'click', function() {
+						count.textContent = increment.increment();
 					});
-				});
 
+					eventService.addEventListener(documentService.getElementById('decrement'), 'click', function() {
+						count.textContent = increment.decrement();
+					});
+
+					eventService.addEventListener(documentService.getElementById('reset'), 'click', function() {
+						count.textContent = increment.reset();
+					});
+
+					nodeListService.forEach(documentService.getElementById('drawerNav').childNodes, addNodeEvent);
+
+					function addNodeEvent(node) {
+						eventService.addEventListener(node, 'click', function() {
+							drawerService.toggle();
+						});
+					}
+				}
 			}
 		};
 		module.exports = app;
