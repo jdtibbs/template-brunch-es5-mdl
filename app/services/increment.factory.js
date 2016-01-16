28ad3e5666d7e4673142b5d35682fb0d2077fe5c
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-01-16 07:53:40 -0600

    implement actual tests.

--- app/services/increment.factory.js
+++ app/services/increment.factory.js
@@ -1,25 +1,25 @@
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		var factory = {
-			create: function() {
-				var count = 0;
+	var factory = {
+		create: function() {
+			var count = 0;
 
-				return {
-					decrement: function() {
-						return --count;
-					},
-					increment: function() {
-						return ++count;
-					},
-					reset: function() {
-						count = 0;
-						return count;
-					}
+			return {
+				decrement: function() {
+					return --count;
+				},
+				increment: function() {
+					return ++count;
+				},
+				reset: function() {
+					count = 0;
+					return count;
+				}
 
-				};
-			}
-		};
+			};
+		}
+	};
 
-		module.exports = factory;
-	})();
+	module.exports = factory;
+})();
--- app/services/increment.factory.test.js
+++ app/services/increment.factory.test.js
@@ -1,23 +1,27 @@
 var test = require('tape');
+var incrementFactory = require('./increment.factory');
 
+var increment = incrementFactory.create();
 
-test('math test', function(t) {
-	// TODO implement actual test
-	t.plan(2);
+test('increment.factory test increment', function(t) {
+	t.plan(3);
 
-	var x = 1 + 3;
-	t.equal(x, 4);
-
-	x = ++x;
-	t.equal(x, 5);
-
+	t.equal(increment.increment(), 1);
+	t.equal(increment.increment(), 2);
+	t.equal(increment.increment(), 3);
 });
 
-test('spelling test', function(t) {
-	// TODO implement actual test
+test('increment.factory test decrement', function(t) {
+	t.plan(4);
+
+	t.equal(increment.decrement(), 2);
+	t.equal(increment.decrement(), 1);
+	t.equal(increment.decrement(), 0);
+	t.equal(increment.decrement(), -1);
+});
+
+test('increment.factory test reset', function(t) {
 	t.plan(1);
 
-	var x = 'hello';
-	t.equal(x, 'hello');
-
+	t.equal(increment.reset(), 0);
 });
